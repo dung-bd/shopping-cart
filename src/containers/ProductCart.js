93e4278bee06c987c90a37927e0c3d83cb82079d
dung <dungbachdang123@gmail.com>
2021-07-19 17:43:02 +0700

    new3

--- src/containers/ProductCart.js
+++ src/containers/ProductCart.js
@@ -5,7 +5,7 @@ import axios from 'axios'
 import { selectedProduct, removeSelectedProduct, setCart } from '../redux/actions/productActions'
 
  const ProductCart = () =>{
-     
+     const [quantity, setQuantity] = useState(1);
     const product= useSelector(state => state.cart)
     const {image, title, price, category, description} = product;
     const {productId} = useParams()
@@ -33,31 +33,24 @@ import { selectedProduct, removeSelectedProduct, setCart } from '../redux/action
        
     <div className="ui grid container">
         
-        {Object.keys(product).length === 0 ? (<div>...Loading</div>) : (<div className="ui placeholder segment">
-            <div className="ui two column stackable center aligned grid">
-                <div className="ui vertical divider">AND</div>
-                <div className="middle aligned row">
-                    <div className="column lp">
-                        <img className="ui fluid image" src={image} />
-                    </div>
-                    <div className="column rp">
-                        <h1>{title}</h1>
-                        <h2>
-                            <a className="ui teal tag label">${price}</a>
-                        </h2>
-                        <h3 className="ui brown block header">{category}</h3>
-                        <p>{description}</p>
-                         {/* <div className="ui vertical animated button" tabIndex="0" onClick={() => addToCart(product)} > 
-                             <div className="hidden content">
-                                 <i className="shop icon"></i>
-                             </div>
-                             <div className="visible content">Add to Cart</div>
-                          </div> */}
-                          
-                      </div>
-                  </div>
-              </div>
-          </div>)}
+        {Object.keys(product).length === 0 ? (<div>...Loading</div>) : (
+            <div>
+                <h1>{title}- Price: ${price}</h1> 
+                <img className="b" src={image}/>
+                <h1>Quantity: {quantity}</h1>
+                <button onClick={() => setQuantity(quantity + 1)}>
+        Increase
+      </button>
+      
+      <button onClick={() => setQuantity(quantity - 1)}>
+        Decrease
+      </button>
+      <h1>Total price: ${price * quantity}</h1>
+      <button onClick={()=>{alert("Done deal")}}>Submit</button>
+            </div>
+
+            
+        )}
          
          </div>
          )
